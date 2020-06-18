25180f8e2bf1c54db287a6373dd69c6de41d2289
lm <min.liu@mail.dealmoon.com>
2020-06-18 15:52:32 +0800

    Fix:新版Swift 5.2

--- Learning/ViewController.swift
+++ Learning/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Learning
 //
-//  Created by shuilin on 07/06/2017.
-//  Copyright © 2017 xuetangx. All rights reserved.
+//  Created by matelin on 2020/6/18.
+//  Copyright © 2020 shuilin. All rights reserved.
 //
 
 import UIKit
@@ -12,12 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view.
     }
 
 
